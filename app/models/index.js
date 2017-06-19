27d8d4a51475856304711599916cc5999af284cc
lahavas <hos6959@gmail.com>
2017-06-19 12:55:07 +0900

    fix index

--- app/models/index.js
+++ app/models/index.js
@@ -7,18 +7,17 @@ const config = require(__dirname + '/../config/config.json')[env];
 
 const db = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-  var sequelize = new Sequelize(
+const sequelize = config.use_env_variable ?
+  new Sequelize(process.env[config.use_env_variable]) :
+  new Sequelize(
     config.database,
     config.username,
     config.password, {
       'host': config.host,
       'dialect': config.dialect,
-      //'logging': false
-    });
-}
+      // 'logging': false
+    }
+  );
 
 fs
   .readdirSync(__dirname)
