632bba2e2a8b5bd757fabfc39d964cee85c038a0
lahavas <hos6959@gmail.com>
2017-06-19 23:48:14 +0900

    notification temporated file

---
+++ app/middlewares/push.js
@@ -0,0 +1,15 @@
+// const FCM = require('fcm-push');
+// const apiKey = 'API-KEY';
+//
+// const message = {
+//   registration_ids : tokens,
+//   notification : {
+//     title : 'FCM 알림',
+//     text : msg,
+//     icon : 'ic_football'
+//   }
+// };
+//
+// const fcm = new FCM(apiKey);
+//
+// fcm.send(message);
